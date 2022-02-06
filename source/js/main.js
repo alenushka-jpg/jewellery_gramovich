08944c294490a1262aedeb1547c14668252749db
alenushka <alionxlo@gmail.com>
2022-02-06 16:18:11 +0300

    Пишет скрипт для выпадающего меню

--- source/js/main.js
+++ source/js/main.js
@@ -6,6 +6,11 @@
   var closeLogin = document.querySelector('.js-close-login');
   var name = document.querySelector('[name=email-name]');
   var overlay = document.querySelectorAll('.overlay');
+  var header = document.querySelector('.page-header');
+  var burger = document.querySelector('.page-header__burger');
+  var navigationPopup = document.querySelector('.page-header__popup');
+
+  // Открывает модальное окно логин
 
   function showLoginModal() {
     loginModal.classList.add('login-modal--open');
@@ -42,13 +47,29 @@
     });
   }
 
+  //Открывает выпадающее меню
 
+  function showNavigation() {
+    navigationPopup.classList.toggle('page-header__popup--open');
+  }
+
+  function onClickToggle() {
+    showNavigation();
+    header.classList.toggle('page-header--open');
+    bodyHidden();
+  }
+
+  burger.addEventListener('click', onClickToggle);
+
+  // Закрывает модалки при нажатии esc
   window.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
       onCloseClick();
+      onClickToggle()
     }
   });
 
+  // Закрывает модалки при нажатии на overlay
   overlay.forEach(function (v) {
     v.addEventListener('click', function () {
       onCloseClick();
