b9bf937541f4fbf7877338dc0d0c0eb51c891035
alenushka <alionxlo@gmail.com>
2022-02-10 17:33:02 +0300

    Пишет скрипт для слайдера

--- source/js/main.js
+++ source/js/main.js
@@ -10,10 +10,11 @@
   var burger = document.querySelector('.page-header__burger');
   var navigationPopup = document.querySelector('.page-header__popup');
   var body = document.querySelector('.body');
+
+  var filter = document.querySelector('.filter');
   var filterButton = document.querySelector('.filter__button');
   var filterClose = document.querySelector('.filter__button-close');
   var filterOverlay = document.querySelector('.filter__overlay');
-  var filter = document.querySelector('.filter');
   var questionsButtons = document.querySelectorAll('.js-questions-button');
   var filterButtons = document.querySelectorAll('.filter__accordion-button');
 
@@ -29,17 +30,15 @@
   var labels = bodyMain.querySelectorAll('label');
 
   var breadcrumbs = document.querySelector('.catalog__slider');
-  var buttonsBread = breadcrumbs.querySelectorAll('button');
+
+  if (breadcrumbs) {
+    var buttonsBread = breadcrumbs.querySelectorAll('button');
+  }
 
   var pageFooter = document.querySelector('.page-footer');
   var linksFooter = pageFooter.querySelectorAll('a');
   var buttonFooter = pageFooter.querySelector('button');
   var inputFooter = pageFooter.querySelector('input');
-
-  var prevSlideButton = document.querySelector('.js-slider-prev');
-  var nextSlideButton = document.querySelector('.js-slider-next');
-  var slidesImages = document.querySelector('.js-slider-images');
-  var sliderPagination = document.querySelector('.js-slider-pagination');
 
   function setBlur(e) {
     e.forEach(function (v) {
@@ -51,6 +50,218 @@
     e.forEach(function (v) {
       v.removeAttribute('tabindex');
     });
+  }
+
+  // Слайдер
+  var prevSlideButton = document.querySelector('.js-slider-prev');
+  var nextSlideButton = document.querySelector('.js-slider-next');
+  var slidesRack = document.querySelector('.js-rack');
+  var sliderPagination = document.querySelector('.js-slider-pagination');
+
+  var newInItems = [
+    {width: '270', height: '284', href: '#', src: './img/purple-gemstone-necklace.jpg', srcset: './img/purple-gemstone-necklace.jpg 1x, ./img/purple-gemstone-necklace@2x.jpg 2x', webp: './img/purple-gemstone-necklace.webp 1x, ./img/purple-gemstone-necklace@2x.webp 2x', alt: 'Necklace with amethyst', title: 'Necklace with amethyst', price: '$ 145'},
+    {width: '270', height: '284', href: '#', src: './img/layered-choker-necklace.jpg', srcset: './img/layered-choker-necklace.jpg 1x, ./img/layered-choker-necklace@2x.jpg 2x', webp: './img/layered-choker-necklace.webp 1x, ./img/layered-choker-necklace@2x.webp 2x', alt: 'Layered choker', title: 'Layered choker', price: '$ 115'},
+    {width: '270', height: '284', href: '#', src: './img/dainty-gold-necklace.jpg', srcset: './img/dainty-gold-necklace.jpg 1x, ./img/dainty-gold-necklace@2x.jpg 2x', webp: './img/dainty-gold-necklace.webp 1x, ./img/dainty-gold-necklace@2x.webp 2x', alt: 'Gold chain', title: 'Gold chain', price: '$ 215'},
+    {width: '270', height: '284', href: '#', src: './img/galaxy-earrings.jpg', srcset: './img/galaxy-earrings.jpg 1x, ./img/galaxy-earrings@2x.jpg 2x', webp: './img/galaxy-earrings.webp 1x, ./img/galaxy-earrings@2x.webp 2x', alt: 'Galaxy earrings', title: 'Galaxy earrings', price: '$ 95'},
+
+    {width: '270', height: '284', href: '#', src: './img/purple-gemstone-necklace.jpg', srcset: './img/purple-gemstone-necklace.jpg 1x, ./img/purple-gemstone-necklace@2x.jpg 2x', webp: './img/purple-gemstone-necklace.webp 1x, ./img/purple-gemstone-necklace@2x.webp 2x', alt: 'Necklace with amethyst', title: 'Necklace with amethyst', price: '$ 145'},
+    {width: '270', height: '284', href: '#', src: './img/layered-choker-necklace.jpg', srcset: './img/layered-choker-necklace.jpg 1x, ./img/layered-choker-necklace@2x.jpg 2x', webp: './img/layered-choker-necklace.webp 1x, ./img/layered-choker-necklace@2x.webp 2x', alt: 'Layered choker', title: 'Layered choker', price: '$ 115'},
+    {width: '270', height: '284', href: '#', src: './img/purple-gemstone-necklace.jpg', srcset: './img/purple-gemstone-necklace.jpg 1x, ./img/purple-gemstone-necklace@2x.jpg 2x', webp: './img/purple-gemstone-necklace.webp 1x, ./img/purple-gemstone-necklace@2x.webp 2x', alt: 'Necklace with amethyst', title: 'Necklace with amethyst', price: '$ 145'},
+    {width: '270', height: '284', href: '#', src: './img/galaxy-earrings.jpg', srcset: './img/galaxy-earrings.jpg 1x, ./img/galaxy-earrings@2x.jpg 2x', webp: './img/galaxy-earrings.webp 1x, ./img/galaxy-earrings@2x.webp 2x', alt: 'Galaxy earrings', title: 'Galaxy earrings', price: '$ 95'},
+    {width: '270', height: '284', href: '#', src: './img/galaxy-earrings.jpg', srcset: './img/galaxy-earrings.jpg 1x, ./img/galaxy-earrings@2x.jpg 2x', webp: './img/galaxy-earrings.webp 1x, ./img/galaxy-earrings@2x.webp 2x', alt: 'Galaxy earrings', title: 'Galaxy earrings', price: '$ 95'},
+    {width: '270', height: '284', href: '#', src: './img/dainty-gold-necklace.jpg', srcset: './img/dainty-gold-necklace.jpg 1x, ./img/dainty-gold-necklace@2x.jpg 2x', webp: './img/dainty-gold-necklace.webp 1x, ./img/dainty-gold-necklace@2x.webp 2x', alt: 'Gold chain', title: 'Gold chain', price: '$ 215'},
+    {width: '270', height: '284', href: '#', src: './img/galaxy-earrings.jpg', srcset: './img/galaxy-earrings.jpg 1x, ./img/galaxy-earrings@2x.jpg 2x', webp: './img/galaxy-earrings.webp 1x, ./img/galaxy-earrings@2x.webp 2x', alt: 'Galaxy earrings', title: 'Galaxy earrings', price: '$ 95'},
+  ];
+
+  function createSlider() {
+    var currentPage = 1;
+    var ITEM_PER_SLIDE_DESKTOP = 4;
+    var ITEM_PER_SLIDE_MOBILE = 2;
+    var pages = [];
+    var currentMode = window.outerWidth <= 1023 ? 'mobile' : 'desktop';
+    createPagination();
+
+    function scrollNext() {
+      scrollToPage(currentPage + 1);
+    }
+
+    function scrollPrev() {
+      scrollToPage(currentPage - 1);
+    }
+
+    function scrollToPage(pageNum) {
+      if (pageNum < 1 || pageNum > pages.length) {
+        return;
+      }
+
+      slidesRack.scrollLeft = (pageNum - 1) * (slidesRack.clientWidth + 30);
+
+      if (pages[currentPage - 1]) {
+        pages[currentPage - 1].classList.remove('active');
+      }
+      currentPage = pageNum;
+      pages[currentPage - 1].classList.add('active');
+    }
+
+    disableSliderButton();
+
+    prevSlideButton.addEventListener('click', scrollPrev);
+    nextSlideButton.addEventListener('click', function () {
+      scrollNext();
+      disableSliderButton();
+    });
+
+
+    function disableSliderButton() {
+      if (currentPage === 1) {
+        prevSlideButton.classList.add('disabled');
+      } else {
+        prevSlideButton.classList.remove('disabled');
+      }
+
+      if (currentPage === pages.length) {
+        nextSlideButton.classList.add('disabled');
+      } else {
+        nextSlideButton.classList.remove('disabled');
+      }
+    }
+
+    function createPagination() {
+      var itemsPerSlide = currentMode === 'mobile' ? ITEM_PER_SLIDE_MOBILE : ITEM_PER_SLIDE_DESKTOP;
+      var pagesQuantity = Math.ceil(slidesRack.children.length / itemsPerSlide);
+
+      for (var i = 1; i <= pagesQuantity; i++) {
+        var button = document.createElement('button');
+        button.classList.add('js-breadcrumbs-button');
+        button.setAttribute('type', 'button');
+        button.textContent = i;
+
+        if (window.outerWidth >= 768) {
+          button.addEventListener('click', onPageNumClick(i));
+        }
+
+        pages.push(button);
+        sliderPagination.appendChild(button);
+      }
+
+      scrollToPage(1);
+    }
+
+    function onPageNumClick(pageNum) {
+      return function () {
+        scrollToPage(pageNum);
+      };
+    }
+
+    function destroyPagination() {
+      sliderPagination.innerHTML = '';
+      pages = [];
+    }
+
+    function onResize() {
+      var newMode = window.outerWidth <= 1023 ? 'mobile' : 'desktop';
+
+      if (newMode !== currentMode) {
+        currentMode = newMode;
+        destroyPagination();
+        createPagination();
+      }
+    }
+
+    window.addEventListener('resize', onResize);
+
+    // MOBILE
+    var startTouchEventX = null;
+    var direction = null;
+    var startScrollLeft = 0;
+
+    function onTouchStart(e) {
+      slidesRack.style.scrollBehavior = 'unset';
+      startScrollLeft = slidesRack.scrollLeft;
+      startTouchEventX = e.changedTouches[0].clientX;
+    }
+
+    function onTouchEnd(e) {
+      if (startTouchEventX === null) {
+        return;
+      }
+
+      slidesRack.style.scrollBehavior = 'smooth';
+
+      if (e.changedTouches[0].clientX - startTouchEventX > 0) {
+        direction = 'right';
+      } else {
+        direction = 'left';
+      }
+
+      if (direction === 'left') {
+        scrollNext();
+      }
+
+      if (direction === 'right') {
+        scrollPrev();
+      }
+    }
+
+    function onTouchMove(e) {
+      var delta = e.changedTouches[0].clientX - startTouchEventX;
+      slidesRack.scrollLeft = startScrollLeft - delta;
+    }
+
+    if (currentMode === 'mobile') {
+      slidesRack.addEventListener('touchstart', onTouchStart);
+      slidesRack.addEventListener('touchend', onTouchEnd);
+      slidesRack.addEventListener('touchmove', onTouchMove);
+    }
+  }
+
+  function renderSlides(array) {
+    array.forEach(function (v) {
+      var newSlide = createListEl(v.href, v.webp, v.src, v.srcset, v.title, v.price, v.width, v.height, v.alt);
+      slidesRack.appendChild(newSlide);
+    });
+  }
+
+  if (slidesRack) {
+    renderSlides(newInItems);
+  }
+
+  function createListEl(href, webp, src, srcset, title, price, width, height, alt) {
+    var liEl = document.createElement('li');
+    var linkEl = document.createElement('a');
+    var pictureEl = document.createElement('picture');
+    var imgEl = document.createElement('img');
+    var sourceElWebp = document.createElement('source');
+    var sourceElJpeg = document.createElement('source');
+    var titleEl = document.createElement('h3');
+    var priceEl = document.createElement('p');
+
+    linkEl.setAttribute('href', href);
+
+    sourceElWebp.setAttribute('srcset', webp);
+    sourceElWebp.setAttribute('type', 'image/webp');
+    sourceElJpeg.setAttribute('srcset', srcset);
+    sourceElJpeg.setAttribute('type', 'image/lpeg');
+
+    imgEl.setAttribute('src', src);
+    imgEl.setAttribute('alt', alt);
+    imgEl.setAttribute('width', width);
+    imgEl.setAttribute('height', height);
+    titleEl.textContent = title;
+    priceEl.textContent = price;
+
+    liEl.appendChild(linkEl);
+    linkEl.appendChild(pictureEl);
+    linkEl.appendChild(titleEl);
+    linkEl.appendChild(priceEl);
+    pictureEl.appendChild(sourceElWebp);
+    pictureEl.appendChild(sourceElJpeg);
+    pictureEl.appendChild(imgEl);
+    return liEl;
+  }
+
+  if (prevSlideButton && nextSlideButton && slidesRack && sliderPagination) {
+    createSlider();
   }
 
   // Открывает модальное окно логин
@@ -128,8 +339,10 @@
   }
 
   function onCloseClick() {
-    if (loginModal === 'login-modal--open' && window.outerWidth >= 1023) {
+    if (loginModal !== 'login-modal--open' && window.outerWidth >= 1023) {
       bodyVisible();
+    } else {
+      bodyHidden();
     }
 
     closeLoginModal();
@@ -137,9 +350,8 @@
   }
 
   function onCloseClickMobile() {
-    if (navigationPopup !== 'page-header__popup--open' && loginModal !== 'login-modal--open') {
-      closeLoginModal();
-    }
+    closeLoginModal();
+    bodyHiddenMobile();
   }
 
   for (var i = 0; i < loginButtons.length; i++) {
@@ -217,11 +429,15 @@
   // Открывает фильтр
 
   function showOverlay() {
-    filter.classList.add('filter--open');
+    if (filter) {
+      filter.classList.add('filter--open');
+    }
   }
 
   function hiddenShowOverlay() {
-    filter.classList.remove('filter--open');
+    if (filter) {
+      filter.classList.remove('filter--open');
+    }
   }
 
   function showFilter() {
