3c9181eb9250a5173c13b87145d004afa9c0d854
alenushka <alionxlo@gmail.com>
2022-08-20 12:33:50 +0300

    Обновляет зависимости

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,185 @@
-"use strict";
-
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
-var sass = require("gulp-sass");
+var sass = require('gulp-sass');
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var csso = require("gulp-csso");
+var csso = require("postcss-csso");
 var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
+var htmlmin = require("gulp-htmlmin");
+var terser = require("gulp-terser");
+var squoosh = require("gulp-libsquoosh");
 var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore")
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
+var svgstore = require("gulp-svgstore");
 var del = require("del");
+var sync = require("browser-sync").create();
 
-gulp.task("css", function () {
+// Styles
+
+var styles = function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([ autoprefixer() ]))
-    .pipe(csso())
+    .pipe(postcss([
+      autoprefixer(),
+      csso()
+    ]))
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
-});
+    .pipe(sync.stream());
+}
 
-gulp.task("server", function () {
-  server.init({
-    server: "build/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+exports.styles = styles;
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
-});
+// HTML
 
-gulp.task("refresh", function (done) {
-  server.reload();
-  done();
-});
+var html = function () {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+}
 
-gulp.task("images", function() {
+// Scripts
+
+var scripts = function () {
+  return gulp.src("source/js/*.js")
+    .pipe(terser())
+    .pipe(rename(function (path) {
+      path.basename += ".min";
+    }))
+    .pipe(gulp.dest("build/js"));
+}
+
+exports.scripts = scripts;
+
+// Images
+
+var optimizeImages = function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
+    .pipe(squoosh())
+    .pipe(gulp.dest("build/img"))
+}
 
-    .pipe(gulp.dest("source/img"));
+exports.images = optimizeImages;
 
-});
+var copyImages = function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(gulp.dest("build/img"))
+}
 
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
+exports.images = copyImages;
+
+// WebP
+
+var createWebp = function () {
+  return gulp.src("source/img/**/*.{jpg,png}")
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
-});
+    .pipe(gulp.dest("build/img"))
+}
 
-gulp.task("sprite", function () {
+exports.createWebp = createWebp;
+
+// Sprite
+
+var sprite = function () {
   return gulp.src("source/img/svg/*.svg")
-    .pipe(svgstore({inlineSvg: true}))
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
-});
+}
 
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
-});
+exports.sprite = sprite;
 
-gulp.task("copy", function () {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**",
-    "source//*.ico"
-    ], {
-      base: "source"
-    })
-  .pipe(gulp.dest("build"));
-});
+// Copy
 
-gulp.task("clean", function () {
+var copy = function (done) {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.svg",
+    "!source/img/icons/*.svg",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+}
+
+exports.copy = copy;
+
+// Clean
+
+var clean = function () {
   return del("build");
-});
+};
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
-gulp.task("start", gulp.series("build", "server"));
+// Server
+
+var server = function (done) {
+  sync.init({
+    server: {
+      baseDir: "build"
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+exports.server = server;
+
+// Reload
+
+var reload = function(done) {
+  sync.reload();
+  done();
+}
+
+// Watcher
+
+var watcher = function() {
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/js/*.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(html, reload));
+}
+
+// Build
+
+var build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  )
+);
+
+exports.build = build;
+
+// Default
+
+exports.default = gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
+);
--- source/js/main.js
+++ source/js/main.js
@@ -1,291 +1,291 @@
-const body = document.querySelector('.body');
-const loginButtons = document.querySelectorAll('.js-open-login');
-const loginModal = document.querySelector('.login-modal');
-const closeLogin = document.querySelector('.js-close-login');
-const name = document.querySelector('[name=email-name]');
-const overlay = document.querySelectorAll('.overlay');
-const header = document.querySelector('.page-header');
-const burger = document.querySelector('.page-header__burger');
-const navigationPopup = document.querySelector('.page-header__popup');
-const filter = document.querySelector('.filter');
-const filterButton = document.querySelector('.filter__button');
-const filterClose = document.querySelector('.filter__button-close');
-const filterOverlay = document.querySelector('.filter__overlay');
-const questionsButtons = document.querySelectorAll('.js-questions-button');
-const filterButtons = document.querySelectorAll('.filter__accordion-button');
-const emailLogin = document.querySelector('.js-email-login');
-const passwordLogin = document.querySelector('.js-email-password');
-const resultModal = document.querySelector('.js-result-modal');
-const submitbutton = document.querySelector('.js-login-submit');
-const bodyMain = document.querySelector('.body__main');
-const links = bodyMain.querySelectorAll('a');
-const inputs = bodyMain.querySelectorAll('input');
-const buttons = bodyMain.querySelectorAll('button');
-const labels = bodyMain.querySelectorAll('label');
-const pageFooter = document.querySelector('.page-footer');
-const linksFooter = pageFooter.querySelectorAll('a');
-const buttonFooter = pageFooter.querySelector('button');
-const inputFooter = pageFooter.querySelector('input');
-const breadcrumbs = document.querySelector('.catalog__slider');
+var body = document.querySelector('.body');
+var loginButtons = document.querySelectorAll('.js-open-login');
+var loginModal = document.querySelector('.login-modal');
+var closeLogin = document.querySelector('.js-close-login');
+var name = document.querySelector('[name=email-name]');
+var overlay = document.querySelectorAll('.overlay');
+var header = document.querySelector('.page-header');
+var burger = document.querySelector('.page-header__burger');
+var navigationPopup = document.querySelector('.page-header__popup');
+var filter = document.querySelector('.filter');
+var filterButton = document.querySelector('.filter__button');
+var filterClose = document.querySelector('.filter__button-close');
+var filterOverlay = document.querySelector('.filter__overlay');
+var questionsButtons = document.querySelectorAll('.js-questions-button');
+var filterButtons = document.querySelectorAll('.filter__accordion-button');
+var emailLogin = document.querySelector('.js-email-login');
+var passwordLogin = document.querySelector('.js-email-password');
+var resultModal = document.querySelector('.js-result-modal');
+var submitbutton = document.querySelector('.js-login-submit');
+var bodyMain = document.querySelector('.body__main');
+var links = bodyMain.querySelectorAll('a');
+var inputs = bodyMain.querySelectorAll('input');
+var buttons = bodyMain.querySelectorAll('button');
+var labels = bodyMain.querySelectorAll('label');
+var pageFooter = document.querySelector('.page-footer');
+var linksFooter = pageFooter.querySelectorAll('a');
+var buttonFooter = pageFooter.querySelector('button');
+var inputFooter = pageFooter.querySelector('input');
+var breadcrumbs = document.querySelector('.catalog__slider');
 
   if (breadcrumbs) {
-    const buttonsBread = breadcrumbs.querySelectorAll('button');
+    var buttonsBread = breadcrumbs.querySelectorAll('button');
   }
 
-  function setBlur(e) {
-    e.forEach(function (v) {
-      v.setAttribute('tabindex', '-1');
-    });
-  }
+  // function setBlur(e) {
+  //   e.forEach(function (v) {
+  //     v.setAttribute('tabindex', '-1');
+  //   });
+  // }
 
-  function removeBlur(e) {
-    e.forEach(function (v) {
-      v.removeAttribute('tabindex');
-    });
-  }
+  // function removeBlur(e) {
+  //   e.forEach(function (v) {
+  //     v.removeAttribute('tabindex');
+  //   });
+  // }
 
-  // Открывает модальное окно логин
+  // // Открывает модальное окно логин
 
-  function showLoginModal() {
-    loginModal.classList.add('login-modal--open');
-    name.focus();
-    setBlur(links);
-    setBlur(inputs);
-    setBlur(buttons);
-    setBlur(labels);
-  }
+  // function showLoginModal() {
+  //   loginModal.classList.add('login-modal--open');
+  //   name.focus();
+  //   setBlur(links);
+  //   setBlur(inputs);
+  //   setBlur(buttons);
+  //   setBlur(labels);
+  // }
 
-  function closeLoginModal() {
-    loginModal.classList.remove('login-modal--open');
-    removeBlur(links);
-    removeBlur(inputs);
-    removeBlur(buttons);
-    removeBlur(labels);
-  }
+  // function closeLoginModal() {
+  //   loginModal.classList.remove('login-modal--open');
+  //   removeBlur(links);
+  //   removeBlur(inputs);
+  //   removeBlur(buttons);
+  //   removeBlur(labels);
+  // }
 
-  function onOpenClick() {
-    showLoginModal();
+  // function onOpenClick() {
+  //   showLoginModal();
 
-    if (loginModal !== 'login-modal--open' && window.outerWidth >= 1023) {
-      bodyHidden();
-    }
-  }
+  //   if (loginModal !== 'login-modal--open' && window.outerWidth >= 1023) {
+  //     bodyHidden();
+  //   }
+  // }
 
-  function validation(email, password) {
-    const valid = true;
-    const newLocal = email.trim() === 0 || password === true;
+  // function validation(email, password) {
+  //   var valid = true;
+  //   var newLocal = email.trim() === 0 || password === true;
 
-    if (newLocal) {
-      valid = false;
-    } else {
-      submitbutton.addEventListener('click', showQuestionsResult);
-    }
+  //   if (newLocal) {
+  //     valid = false;
+  //   } else {
+  //     submitbutton.addEventListener('click', showQuestionsResult);
+  //   }
 
-    return valid;
-  }
+  //   return valid;
+  // }
 
-  function showQuestionsResult() {
-    resultModal.style.display = 'flex';
-  }
+  // function showQuestionsResult() {
+  //   resultModal.style.display = 'flex';
+  // }
 
-  function localStorageData(data) {
-    localStorage.setItem('email', data.userEmail);
-  }
+  // function localStorageData(data) {
+  //   localStorage.setItem('email', data.userEmail);
+  // }
 
-  function onSubmitModalForm(e) {
-    e.preventDefault();
-    const userEmail = emailLogin.value;
-    const userPassword = passwordLogin.value;
+  // function onSubmitModalForm(e) {
+  //   e.preventDefault();
+  //   var userEmail = emailLogin.value;
+  //   var userPassword = passwordLogin.value;
 
-    if (validation(userEmail, userPassword)) {
-      resultModal.style.display = 'flex';
+  //   if (validation(userEmail, userPassword)) {
+  //     resultModal.style.display = 'flex';
 
-      localStorageData({
-        userEmail: userEmail
-      });
-    }
-  }
+  //     localStorageData({
+  //       userEmail: userEmail
+  //     });
+  //   }
+  // }
 
-  if (loginModal) {
-    loginModal.addEventListener('submit', onSubmitModalForm);
-  }
+  // if (loginModal) {
+  //   loginModal.addEventListener('submit', onSubmitModalForm);
+  // }
 
-  function bodyHidden() {
-    body.classList.add('body--hidden');
-  }
+  // function bodyHidden() {
+  //   body.classList.add('body--hidden');
+  // }
 
-  function bodyVisible() {
-    body.classList.remove('body--hidden');
-  }
+  // function bodyVisible() {
+  //   body.classList.remove('body--hidden');
+  // }
 
-  function onCloseClick() {
-    if (loginModal !== 'login-modal--open' && window.outerWidth >= 1023) {
-      bodyVisible();
-    }
+  // function onCloseClick() {
+  //   if (loginModal !== 'login-modal--open' && window.outerWidth >= 1023) {
+  //     bodyVisible();
+  //   }
 
-    closeLoginModal();
-  }
+  //   closeLoginModal();
+  // }
 
-  function onCloseClickMobile() {
-    closeLoginModal();
-    // bodyHiddenMobile();
-    if (navigationPopup !== 'page-header__popup--open' && loginModal !== 'login-modal--open') {
-      bodyHidden();
-    }
-  }
+  // function onCloseClickMobile() {
+  //   closeLoginModal();
+  //   // bodyHiddenMobile();
+  //   if (navigationPopup !== 'page-header__popup--open' && loginModal !== 'login-modal--open') {
+  //     bodyHidden();
+  //   }
+  // }
 
-  for (const i = 0; i < loginButtons.length; i++) {
-    const element = loginButtons[i];
+  // for (var i = 0; i < loginButtons.length; i++){
+  //   var element = loginButtons[i];
 
-    element.addEventListener('click', function () {
-      onOpenClick();
-    });
-  }
+  //   element.addEventListener('click', function () {
+  //     onOpenClick();
+  //   });
+  // }
 
-  if (closeLogin) {
-    closeLogin.addEventListener('click', onCloseClick);
-  }
+  // if (closeLogin) {
+  //   closeLogin.addEventListener('click', onCloseClick);
+  // }
 
-  // Открывает выпадающее меню
+  // // Открывает выпадающее меню
 
-  function showNavigation() {
-    navigationPopup.classList.toggle('page-header__popup--open');
-  }
+  // function showNavigation() {
+  //   navigationPopup.classList.toggle('page-header__popup--open');
+  // }
 
-  function showHeader() {
-    header.classList.toggle('page-header--open');
-  }
+  // function showHeader() {
+  //   header.classList.toggle('page-header--open');
+  // }
 
-  function bodyHiddenMobile() {
-    if (body !== 'body') {
-      body.classList.toggle('body--hidden');
-    }
-  }
+  // function bodyHiddenMobile() {
+  //   if (body !== 'body') {
+  //     body.classList.toggle('body--hidden');
+  //   }
+  // }
 
-  function onClickToggle() {
-    showNavigation();
-    showHeader();
-    bodyHiddenMobile();
-  }
+  // function onClickToggle() {
+  //   showNavigation();
+  //   showHeader();
+  //   bodyHiddenMobile();
+  // }
 
-  burger.addEventListener('click', onClickToggle);
+  // burger.addEventListener('click', onClickToggle);
 
-  // Закрывает модалки при нажатии esc
-  window.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-      onCloseClick();
-    }
-  });
+  // // Закрывает модалки при нажатии esc
+  // window.addEventListener('keydown', function (event) {
+  //   if (event.key === 'Escape') {
+  //     onCloseClick();
+  //   }
+  // });
 
-  // Закрывает модалки при нажатии на overlay
-  overlay.forEach(function (v) {
-    v.addEventListener('click', function () {
-      onCloseClickMobile();
-      onCloseClick();
-      onClickCloseFilter();
-    });
-  });
+  // // Закрывает модалки при нажатии на overlay
+  // overlay.forEach(function (v) {
+  //   v.addEventListener('click', function () {
+  //     onCloseClickMobile();
+  //     onCloseClick();
+  //     onClickCloseFilter();
+  //   });
+  // });
 
-  // Аккордеон вопросы
+  // // Аккордеон вопросы
 
-  questionsButtons.forEach(function (item) {
-    item.addEventListener('click', function () {
-      item.parentNode.classList.toggle('questions__item--open');
-    });
-  });
+  // questionsButtons.forEach(function (item) {
+  //   item.addEventListener('click', function () {
+  //     item.parentNode.classList.toggle('questions__item--open');
+  //   });
+  // });
 
-  // Аккордеон фильтр
+  // // Аккордеон фильтр
 
-  filterButtons.forEach(function (array) {
-    array.addEventListener('click', function () {
-      const panel = array.nextElementSibling;
-      const lastElement = array.lastElementChild;
+  // filterButtons.forEach(function (array) {
+  //   array.addEventListener('click', function () {
+  //     var panel = array.nextElementSibling;
+  //     var lastElement = array.lastElementChild;
 
-      panel.classList.toggle('filter__accordion--active');
-      lastElement.classList.toggle('filter__svg--active');
-    });
-  });
+  //     panel.classList.toggle('filter__accordion--active');
+  //     lastElement.classList.toggle('filter__svg--active');
+  //   });
+  // });
 
-  // Открывает фильтр
+  // // Открывает фильтр
 
-  function showOverlay() {
-    if (filter) {
-      filter.classList.add('filter--open');
-    }
-  }
+  // function showOverlay() {
+  //   if (filter) {
+  //     filter.classList.add('filter--open');
+  //   }
+  // }
 
-  function hiddenShowOverlay() {
-    if (filter) {
-      filter.classList.remove('filter--open');
-    }
-  }
+  // function hiddenShowOverlay() {
+  //   if (filter) {
+  //     filter.classList.remove('filter--open');
+  //   }
+  // }
 
-  function showFilter() {
-    filterOverlay.classList.add('filter__overlay--open');
-    setBlur(buttonsBread);
-    setBlur(linksFooter);
-    buttonFooter.setAttribute('tabindex', '-1');
-    inputFooter.setAttribute('tabindex', '-1');
+  // function showFilter() {
+  //   filterOverlay.classList.add('filter__overlay--open');
+  //   setBlur(buttonsBread);
+  //   setBlur(linksFooter);
+  //   buttonFooter.setAttribute('tabindex', '-1');
+  //   inputFooter.setAttribute('tabindex', '-1');
 
-  }
+  // }
 
-  function closeFilter() {
-    if (filterOverlay) {
-      filterOverlay.classList.remove('filter__overlay--open');
-      removeBlur(buttonsBread);
-      removeBlur(linksFooter);
-      buttonFooter.removeAttribute('tabindex');
-      inputFooter.removeAttribute('tabindex');
-    }
-  }
+  // function closeFilter() {
+  //   if (filterOverlay) {
+  //     filterOverlay.classList.remove('filter__overlay--open');
+  //     removeBlur(buttonsBread);
+  //     removeBlur(linksFooter);
+  //     buttonFooter.removeAttribute('tabindex');
+  //     inputFooter.removeAttribute('tabindex');
+  //   }
+  // }
 
-  function onClickFilter() {
-    showFilter();
-    bodyHidden();
-    showOverlay();
-  }
+  // function onClickFilter() {
+  //   showFilter();
+  //   bodyHidden();
+  //   showOverlay();
+  // }
 
-  function onClickCloseFilter() {
-    closeFilter();
-    bodyVisible();
-    hiddenShowOverlay();
-  }
+  // function onClickCloseFilter() {
+  //   closeFilter();
+  //   bodyVisible();
+  //   hiddenShowOverlay();
+  // }
 
-  if (filterButton) {
-    filterButton.addEventListener('click', onClickFilter);
-  }
+  // if (filterButton) {
+  //   filterButton.addEventListener('click', onClickFilter);
+  // }
 
-  if (filterClose) {
-    filterClose.addEventListener('click', onClickCloseFilter);
-  }
+  // if (filterClose) {
+  //   filterClose.addEventListener('click', onClickCloseFilter);
+  // }
 
-  // Закрывает модалки при нажатии esc
-  window.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape') {
-      onClickCloseFilter();
-    }
-  });
+  // // Закрывает модалки при нажатии esc
+  // window.addEventListener('keydown', function (e) {
+  //   if (e.key === 'Escape') {
+  //     onClickCloseFilter();
+  //   }
+  // });
 
-  const catalogSwiper = document.querySelector('.new-jewelery__catalog');
-  const breakpoint = window.matchMedia('(max-width: 768px)');
-  let swiper;
+  // var catalogSwiper = document.querySelector('.new-jewelery__catalog');
+  // var breakpoint = window.matchMedia('(max-width: 768px)');
+  // let swiper;
 
-  const initSwiper = () => {
-    if (swiper) {
-      swiper.destroy(true, true);
-    }
+  // var initSwiper = () => {
+  //   if (swiper) {
+  //     swiper.destroy(true, true);
+  //   }
 
-    swiper = new Swiper(serviceSwiper, {
-      loop: true,
-      slidesPerView: 'auto',
+  //   swiper = new Swiper(serviceSwiper, {
+  //     loop: true,
+  //     slidesPerView: 'auto',
 
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-        renderFraction: function (currentClass, totalClass) {
-          return '<span class="' + currentClass + '"></span>' +
-          '<span class="' + totalClass + '"></span>';
-        },
-      },
-    });
-  };
+  //     pagination: {
+  //       el: '.swiper-pagination',
+  //       type: 'fraction',
+  //       renderFraction: function (currentClass, totalClass) {
+  //         return '<span class="' + currentClass + '"></span>' +
+  //         '<span class="' + totalClass + '"></span>';
+  //       },
+  //     },
+  //   });
+  // };
